8c9852b2e2faae5d85079b0337e73c9120bb5d93
Andrea Contina <acontina@outlook.com>
2024-11-12 12:01:50 -0600

    Second Version

--- DAS.R
+++ DAS.R
@@ -1,5 +1,5 @@
-# R Script Version 01
-data <- c(1, 3, 4, 5)
+# R Script Version 02 - Add more data
+data <- c(1, 3, 4, 5, 374, 596, 234, 54, 221, 2343, 568, 444)
 mean_data <- mean(data)
 print(mean_data)
 # End of Script
